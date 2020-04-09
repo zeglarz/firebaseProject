b7a7798a3dec31e55dc6b9be52032c50c30ef222
Konrad Rudnicki <zeglarz@gmail.com>
2020-04-09 17:27:06 +0200

    middleware implemented

--- functions/index.js
+++ functions/index.js
@@ -33,11 +33,38 @@ app.get('/screams', (req, res) => {
         .catch(err => console.log(err));
 });
 
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found');
+        return res.status(403).json({ error: 'Unauthorized' });
+    }
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log(decodedToken);
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token', err);
+            return res.status(403).json(err);
+        });
+};
 
-app.post('/scream', (req, res) => {
+// Post a scream
+app.post('/scream', FBAuth, (req, res) => {
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
     db
@@ -112,7 +139,6 @@ app.post('/signup', (req, res) => {
         });
 });
 // Login
-
 app.post('/login', (req, res) => {
     const user = {
         email: req.body.email,
@@ -129,6 +155,9 @@ app.post('/login', (req, res) => {
         .then(token => res.json({ token }))
         .catch(err => {
             console.error(err);
+            if (err.code === 'auth/user-not-found' || 'auth/wrong-password') {
+                return res.status(403).json({ error: 'invalid credentials' });
+            }
             return res.status(500).json({ error: err.code });
         });
 });
