c628c7cd896b84e96eb99a04c2c18dc1ead9a1c2
Konrad Rudnicki <zeglarz@gmail.com>
2020-04-11 16:17:49 +0200

    unliking a scream now possible

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -139,5 +139,36 @@ exports.likeScream = (req, res) => {
 
 // Unlike a scream
 exports.unlikeScream = (req, res) => {
+    const likeDocument = db.collection('likes').where('userHandle', '==', req.user.handle)
+        .where('screamId', '==', req.params.screamId).limit(1);
+    const screamDocument = db.doc(`screams/${req.params.screamId}`);
 
+    let screamData = {};
+    screamDocument.get()
+        .then(doc => {
+            if (doc.exists) {
+                screamData = doc.data();
+                screamData.screamId = doc.id;
+                return likeDocument.get();
+            } else {
+                return res.status(404).json({ error: 'Scream not found' });
+            }
+        })
+        .then(data => {
+            console.log(data.docs[0].id);
+            if (!data.empty) {
+                return db.doc(`/likes/${data.docs[0].id}`).delete()
+                    .then(() => {
+                        screamData.likeCount--;
+                        return screamDocument.update({ likeCount: screamData.likeCount });
+                    })
+                    .then(() => res.json(screamData));
+            } else {
+                return res.status(400).json({ error: 'Scream already unliked' });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        });
 };
